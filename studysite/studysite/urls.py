62ef5d739c2b78a740d187a158140b277313d4fd
rha6780 <rha4578@gmail.com>
2022-05-21 23:29:08 +0900

    linter 수정  및 postgres 13 버전으로 다운

--- studysite/studysite/urls.py
+++ studysite/studysite/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('main/', include("apps.main.urls")),
+    path("admin/", admin.site.urls),
+    path("main/", include("apps.main.urls")),
 ]
