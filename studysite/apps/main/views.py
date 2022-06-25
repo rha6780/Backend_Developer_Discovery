b6868eac1b758c07d3b28a5854ff0e3723bdab6d
rha6780 <rha4578@gmail.com>
2022-06-25 23:59:44 +0900

    restframework 추가

--- studysite/apps/main/views.py
+++ studysite/apps/main/views.py
@@ -1,8 +1,16 @@
+from tkinter.messagebox import QUESTION
 from django import views
 from django.shortcuts import render
+from .models import Question
+from rest_framework.views import APIView
 
 
 # Create your views here.
 class MainView(views.View):
     def get(self, request):
         return render(request, "main/index.html")
+
+class QuestionList(APIView):
+    def get(self, request):
+        question = Question.objects.all()
+        return question
