554d12ec488091d71a0e9441b9dfdeaf22787cd0
rha6780 <rha4578@gmail.com>
2022-07-09 23:34:41 +0900

    admin 모델 추가

--- studysite/apps/main/admin.py
+++ studysite/apps/main/admin.py
@@ -5,6 +5,6 @@ from .models import Question
 from .models import Score
 
 # Register your models here.
-admin.register(Question)
-admin.register(Answer)
-admin.register(Score)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Score)
