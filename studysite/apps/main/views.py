38bcc8cedbeb5cf26203e358359e8f1e8f3a2e6c
rha6780 <rha4578@gmail.com>
2022-07-09 23:49:02 +0900

    view 구조 변경

--- studysite/apps/main/views.py
+++ studysite/apps/main/views.py
@@ -10,21 +10,24 @@ from .serializers import AnswerSerializer
 from .serializers import QuestionSerializer
 
 
-# Create your views here.
 class MainView(views.View):
     def get(self, request):
         return render(request, "main/index.html")
 
 
 class QuestionListView(APIView):
+    serializer_class = QuestionSerializer
+
     def get(self, request):
         queryset = Question.objects.all()
-        serializer = QuestionSerializer(queryset)
+        serializer = self.serializer_class(queryset)
         return Response(serializer.data)
 
 
 class AnswerListView(APIView):
+    serializer_class = AnswerSerializer
+
     def get(self, request):
         queryset = Answer.objects.all()
-        serializer = AnswerSerializer(queryset)
+        serializer = self.serializer_class(queryset)
         return Response(serializer.data)
