53b36e87356eb0f22d0ea3890bad1b02f5e17626
rha6780 <rha4578@gmail.com>
2022-09-11 15:55:02 +0900

    feat: django-cors-headers 설정 및 써드 파티 앱 설정 분리

--- studysite/studysite/settings/base.py
+++ studysite/studysite/settings/base.py
@@ -31,18 +31,28 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+THIRD_PARTY = [
     'rest_framework',
+    'corsheaders',
+]
+
+APPLICATIONS = [
     'apps.main',
 ]
 
+INSTALLED_APPS = THIRD_PARTY + APPLICATIONS + DJANGO_APPS
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
