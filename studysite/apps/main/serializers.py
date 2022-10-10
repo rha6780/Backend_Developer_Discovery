af4a32346ab98263aa3143379595ac1fab5a0823
rha6780 <rha4578@gmail.com>
2022-10-10 23:11:59 +0900

    feat: Score view, Serializer

--- studysite/apps/main/serializers.py
+++ studysite/apps/main/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from .models import Answer
 from .models import Question
+from .models import Score
 
 
 class QuestionSerializer(serializers.ModelSerializer):
@@ -13,4 +14,9 @@ class QuestionSerializer(serializers.ModelSerializer):
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
+        fields = "__all__"
+
+class ScoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Score
         fields = "__all__"
--- studysite/apps/main/views.py
+++ studysite/apps/main/views.py
@@ -4,9 +4,10 @@ from rest_framework.views import APIView
 from django import views
 from django.shortcuts import render
 
-from .models import Answer
+from .models import Answer, Score
 from .models import Question
 from .serializers import AnswerSerializer
+from .serializers import ScoreSerializer
 from .serializers import QuestionSerializer
 
 
@@ -31,5 +32,14 @@ class AnswerListView(APIView):
 
     def get(self, request, id):
         queryset = queryset = self.model.objects.filter(question_id=id)
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+class ScoreView(APIView):
+    model = Score
+    serializer_class = ScoreSerializer
+
+    def get(self, request, name):
+        queryset = queryset = self.model.objects.filter(name = name)
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
