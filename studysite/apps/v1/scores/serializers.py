4fb51842ecc33ba375ebf539b8140baf248d984a
rha6780 <rha4578@gmail.com>
2023-01-24 12:10:44 +0900

    fix: 불필요한 코드 제거

--- studysite/apps/v1/scores/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from ...db.scores.models import Score
-
-
-class ScoreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Score
-        fields = "__all__"
--- studysite/apps/v1/scores/urls.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from django.http import request
-from django.urls import path
-
-urlpatterns = []
