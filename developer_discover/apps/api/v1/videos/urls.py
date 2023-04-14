06b0c116f061cf0a779597b63e17517e9c10b253
rha6780 <rha4578@gmail.com>
2023-04-14 21:49:08 +0900

    fix: video create api 추가

--- developer_discover/apps/api/v1/videos/urls.py
+++ developer_discover/apps/api/v1/videos/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
-from .views import VideoListView, VideoUpdateView
+from .views import VideoListView, VideoCreateView
 
 
 urlpatterns = [
-    path("list", VideoListView.as_view()),
-    path("update", VideoUpdateView.as_view(), name="update"),
+    path("list", VideoListView.as_view(), name="list"),
+    path("create", VideoCreateView.as_view(), name="create"),
 ]
--- developer_discover/apps/api/v1/videos/views.py
+++ developer_discover/apps/api/v1/videos/views.py
@@ -2,8 +2,7 @@ from django.core.paginator import Paginator
 
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import VideoListSerializer, VideoUpdateInputSerializer
@@ -18,12 +17,13 @@ class VideoListView(ListAPIView):
     paginate_by = 10
 
 
-class VideoUpdateView(APIView):
+class VideoCreateView(CreateAPIView):
     model = Video
     serializer_class = VideoUpdateInputSerializer
     permission_classes = [IsAuthenticated]
 
-    def update(self, request):
-        params = VideoUpdateInputSerializer(request.query_params)
-        params.is_valid(raise_exception=True)
-        Video.objects.create(title="title", introduction="", youtube_link="", user=self.request.user)
+    def post(self, request, *args, **kwargs):
+        payload = request.data
+        payload["user"] = request.user
+
+        serializer = self.get_serializer(data=payload)
