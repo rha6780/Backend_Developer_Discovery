8e0e8cdde05010e1b8b441ff8a8847e06a7f0e6c
rha6780 <rha4578@gmail.com>
2023-04-26 14:09:17 +0900

    fix: test settings DB HOST localhost로 변경

--- developer_discover/developer_discover/settings/test.py
+++ developer_discover/developer_discover/settings/test.py
@@ -1,13 +1,14 @@
 from .base import *
 
 SECRET_KEY = "github_test_secret_key"
+# DATABASE_URL="postgres://postgres:postgres@localhost:5432/test_db"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "test_db",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "db",
+        "HOST": "localhost",
         "PORT": "5432",
     }
 }
