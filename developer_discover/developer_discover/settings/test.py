f810bc2ec13e9c71b099c6e2f2e34a0637298ce6
rha6780 <rha4578@gmail.com>
2023-04-26 14:11:24 +0900

    fix: test settings 에 databse_url 설정

--- developer_discover/developer_discover/settings/test.py
+++ developer_discover/developer_discover/settings/test.py
@@ -1,14 +1,14 @@
 from .base import *
 
 SECRET_KEY = "github_test_secret_key"
-# DATABASE_URL="postgres://postgres:postgres@localhost:5432/test_db"
+DATABASE_URL="postgres://postgres:postgres@localhost:5432/test_db"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "test_db",
         "USER": "postgres",
         "PASSWORD": "postgres",
-        "HOST": "localhost",
+        "HOST": "127.0.0.1",
         "PORT": "5432",
     }
 }
