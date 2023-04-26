bdcd0e0a3fe561c6d42a2a6c79c1d6e9ad8fd880
rha6780 <rha4578@gmail.com>
2023-04-26 14:13:46 +0900

    fix: test settings 오타 수정

--- developer_discover/developer_discover/settings/test.py
+++ developer_discover/developer_discover/settings/test.py
@@ -1,11 +1,11 @@
 from .base import *
 
 SECRET_KEY = "github_test_secret_key"
-DATABASE_URL="postgres://postgres:postgres@localhost:5432/test_db"
+DATABASE_URL="postgres://postgres:postgres@localhost:5432/github_actions"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test_db",
+        "NAME": "github_actions",
         "USER": "postgres",
         "PASSWORD": "postgres",
         "HOST": "127.0.0.1",
