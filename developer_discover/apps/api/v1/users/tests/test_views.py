b65c9b5624a1e02db7ac6cf12f7fe443b50d9bc4
rha6780 <rha4578@gmail.com>
2023-06-07 19:47:01 +0900

    feat: current user view test add

--- developer_discover/apps/api/v1/users/tests/test_views.py
+++ developer_discover/apps/api/v1/users/tests/test_views.py
@@ -7,6 +7,29 @@ from rest_framework_simplejwt.tokens import RefreshToken
 User = get_user_model()
 
 
+class CurrentUserViewTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.client = APIClient()
+        cls.user = User.objects.create_user(email="test@test.com", password="test9090", name="test")
+
+    def test_user_is_not_exist(self):
+        res = self.client.get(reverse("current"), format="json")
+        self.assertEqual(res.status_code, 401)
+
+    def test_user_is_exist(self):
+        client = APIClient()
+        request_factory = APIRequestFactory()
+        refresh = RefreshToken.for_user(self.user)
+        request = request_factory.get(reverse("email"))
+
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+        response = client.get(reverse("current"), format="json")
+        force_authenticate(request, user=self.user)
+
+        self.assertEqual(response.status_code, 200)
+
+
 # https://stackoverflow.com/questions/47576635/django-rest-framework-jwt-unit-test
 class ChangeEmailViewTestCase(TestCase):
     @classmethod
@@ -37,3 +60,13 @@ class ChangeEmailViewTestCase(TestCase):
         force_authenticate(request, user=self.user)
 
         self.assertEqual(response.status_code, 400)
+
+
+# TODO: 로직, url 추가 후 작성
+# class ChangePasswordViewTestCase(TestCase):
+#     @classmethod
+#     def setUpTestData(cls):
+#         cls.request_factory = APIRequestFactory()
+#         cls.user = User.objects.create_user(email="test@test.com", password="test9090", name="test")
+#         cls.valid_email_data = {"password": "test1@test.com"}
+#         cls.invalid_email_data = {"password": ""}
