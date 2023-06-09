299ef854d7f06506ef355c60887c9639b2fc0eb1
rha6780 <rha4578@gmail.com>
2023-06-09 14:56:29 +0900

    feat: url test 추가

--- developer_discover/apps/api/accounts/tests/test_urls.py
+++ developer_discover/apps/api/accounts/tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.test.testcases import TestCase
 from django.urls import reverse, resolve
 
-from ..views import UserSignUpView, UserSignInView
+from ..views import UserSignUpView, UserSignInView, UserEmailConfirmView, UserPasswordResetView
 
 
 class AccountUrlsTestCase(TestCase):
@@ -12,3 +12,11 @@ class AccountUrlsTestCase(TestCase):
     def test_signin_url_is_resolved(self):
         url = reverse("sign-in")
         self.assertEquals(resolve(url).func.view_class, UserSignInView)
+
+    def test_email_check_url_is_resolved(self):
+        url = reverse("email-check")
+        self.assertEquals(resolve(url).func.view_class, UserEmailConfirmView)
+
+    def test_reset_password_url_is_resolved(self):
+        url = reverse("reset-password")
+        self.assertEquals(resolve(url).func.view_class, UserPasswordResetView)
