28ace7a6069649f6cab5debfe468ba83122373e5
rha6780 <rha4578@gmail.com>
2023-06-16 23:24:05 +0900

    feat: manage.py 에서 prod 로 변경

--- developer_discover/developer_discover/settings/prod.py
+++ developer_discover/developer_discover/settings/prod.py
@@ -2,16 +2,16 @@ import environ
 
 from .base import *
 
-environ.Env.read_env(BASE_DIR.parent / ".envs" / ".prod")
+# environ.Env.read_env(BASE_DIR.parent / ".envs" / ".prod")
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ["SECRET_KEY"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("POSTGRES_DB"),
-        "USER": env("POSTGRES_USER"),
-        "PASSWORD": env("POSTGRES_PASSWORD"),
-        "HOST": env("POSTGRES_HOST"),
-        "PORT": env("POSTGRES_PORT"),
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
     }
 }
