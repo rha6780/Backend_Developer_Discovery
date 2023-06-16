a7fa35c971a77541317339db113604402581cae3
rha6780 <rha4578@gmail.com>
2023-06-17 00:07:27 +0900

    feat: collectstatic 추가

--- developer_discover/developer_discover/settings/base.py
+++ developer_discover/developer_discover/settings/base.py
@@ -169,7 +169,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = ()
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
