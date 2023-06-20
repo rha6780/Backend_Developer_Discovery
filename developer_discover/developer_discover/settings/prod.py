026a6a0f896d36512601755ae6ee3f5b3ea789da
rha6780 <rha4578@gmail.com>
2023-06-20 16:04:36 +0900

    fix: os.environ 말고 envs 폴더에서 변수읽어오도록 수정

--- developer_discover/developer_discover/settings/prod.py
+++ developer_discover/developer_discover/settings/prod.py
@@ -2,25 +2,27 @@ import environ
 
 from .base import *
 
-# environ.Env.read_env(BASE_DIR.parent / ".envs" / ".prod")
+environ.Env.read_env(BASE_DIR.parent / ".envs" / ".prod")
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = env("SECRET_KEY")
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": os.environ["POSTGRES_PORT"],
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
     }
 }
 
+GITHUB_ID = env("GITHUB_ID")
+GITHUB_SECRET = env("GITHUB_SECRET")
 
-EMAIL_HOST = os.environ["EMAIL_HOST"]
+EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_PORT = 465
-EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
