62334b557c8e366f13c093aebbb7a8b600055f50
rha6780 <rha4578@gmail.com>
2023-06-20 13:56:21 +0900

    fix: after, before install script 추가

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -4,4 +4,4 @@ python3 developer_discover/manage.py db_connection
 python3 developer_discover/manage.py makemigrations
 python3 developer_discover/manage.py migrate
 
-python3 developer_discover/manage.py runserver 0.0.0.0:8000
+python3 developer_discover/manage.py runserver 0.0.0.0:80
