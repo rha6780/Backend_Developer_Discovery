1236437866a455b70186dd1b0d9b438d95086c83
rha6780 <rha4578@gmail.com>
2023-06-26 18:03:21 +0900

    fix: docker 예전 컨테이너가 계속 남아있는 이슈 해결

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,8 +4,9 @@
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository
 
 # Docker 컨테이너 정지 및 삭제
-docker stop developer_discovery
-docker rm developer_discovery
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
 
 # ECR 이미지 가져오기
 docker pull 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository:latest
