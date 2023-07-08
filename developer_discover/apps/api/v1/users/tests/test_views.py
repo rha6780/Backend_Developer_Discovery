6ff10801080bc997b3b3ccd55848ec694a396cc3
rha6780 <rha4578@gmail.com>
2023-07-08 19:59:57 +0900

    fix: media url 수정

--- developer_discover/apps/api/v1/users/tests/test_views.py
+++ developer_discover/apps/api/v1/users/tests/test_views.py
@@ -37,7 +37,7 @@ class UserViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(
             response.data,
-            {"id": self.user.id, "email": "test@test.com", "name": "test", "image": "/media/user_icon.png"},
+            {"id": self.user.id, "email": "test@test.com", "name": "test", "image": "/user_icon.png"},
         )
 
     def test_valid_changed_data(self):
--- developer_discover/developer_discover/settings/base.py
+++ developer_discover/developer_discover/settings/base.py
@@ -170,7 +170,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 S3_MEDIA_PATH = "media/"
