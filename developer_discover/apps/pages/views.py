fd3ce4c6c89e09a14fb74407a30f673c09262968
rha6780 <rha4578@gmail.com>
2023-07-10 10:04:02 +0900

    fix: render method fix

--- developer_discover/apps/pages/views.py
+++ developer_discover/apps/pages/views.py
@@ -1,21 +1,22 @@
 from django_nextjs.render import render_nextjs_page_sync
+from django_nextjs.render import render_nextjs_page
 
 
 def index(request):
-    return render_nextjs_page_sync(request)
+    return render_nextjs_page(request)
 
 
 def signin(request):
-    return render_nextjs_page_sync(request)
+    return render_nextjs_page(request)
 
 
 def signup(request):
-    return render_nextjs_page_sync(request)
+    return render_nextjs_page(request)
 
 
 def profile(request):
-    return render_nextjs_page_sync(request)
+    return render_nextjs_page(request)
 
 
 def withdrawal(request):
-    return render_nextjs_page_sync(request)
+    return render_nextjs_page(request)
