06614aef14ebebd3d4477dea8e1bc8fd12595968
rha6780 <rha4578@gmail.com>
2023-07-10 10:21:31 +0900

    fix: method 수정

--- developer_discover/apps/pages/views.py
+++ developer_discover/apps/pages/views.py
@@ -1,22 +1,21 @@
 from django_nextjs.render import render_nextjs_page_sync
-from django_nextjs.render import render_nextjs_page
 
 
 def index(request):
-    return render_nextjs_page(request)
+    return render_nextjs_page_sync(request)
 
 
 def signin(request):
-    return render_nextjs_page(request)
+    return render_nextjs_page_sync(request)
 
 
 def signup(request):
-    return render_nextjs_page(request)
+    return render_nextjs_page_sync(request)
 
 
 def profile(request):
-    return render_nextjs_page(request)
+    return render_nextjs_page_sync(request)
 
 
 def withdrawal(request):
-    return render_nextjs_page(request)
+    return render_nextjs_page_sync(request)
