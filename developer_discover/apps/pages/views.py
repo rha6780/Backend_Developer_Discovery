e2d6d59344a17ba0f92485613cd43db340aea0c3
rha6780 <rha4578@gmail.com>
2023-07-10 10:14:28 +0900

    fix: 기존 방식으로 롤백

--- developer_discover/apps/pages/views.py
+++ developer_discover/apps/pages/views.py
@@ -2,21 +2,21 @@ from django_nextjs.render import render_nextjs_page_sync
 from django_nextjs.render import render_nextjs_page
 
 
-async def index(request):
-    return await render_nextjs_page(request)
+def index(request):
+    return render_nextjs_page(request)
 
 
-async def signin(request):
-    return await render_nextjs_page(request)
+def signin(request):
+    return render_nextjs_page(request)
 
 
-async def signup(request):
-    return await render_nextjs_page(request)
+def signup(request):
+    return render_nextjs_page(request)
 
 
-async def profile(request):
-    return await render_nextjs_page(request)
+def profile(request):
+    return render_nextjs_page(request)
 
 
-async def withdrawal(request):
-    return await render_nextjs_page(request)
+def withdrawal(request):
+    return render_nextjs_page(request)
