58da0965f736f8204fcf57255e3b6efab4e7ce4e
rha6780 <rha4578@gmail.com>
2023-07-12 20:26:14 +0900

    fix: front app image pull

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,15 +3,15 @@
 
 # Deprecated
 # # ECR 로그인
-# aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository
+aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository
 
 # # Docker 컨테이너 정지 및 삭제
-# docker stop $(docker ps -a -q)
-# docker rm $(docker ps -a -q)
-# docker rmi $(docker images -q)
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
 
 # # ECR 이미지 가져오기
-# docker pull 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository:latest
+docker pull 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository:latest
 
 # # Docker 컨테이너 실행
 # docker run -d -it --name developer_discovery -p 80:80 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository:latest
