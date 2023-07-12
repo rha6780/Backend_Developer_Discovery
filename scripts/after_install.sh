1a49afe053481ee71d1848327b8d7fdb18627730
rha6780 <rha4578@gmail.com>
2023-07-12 19:58:14 +0900

    feat: after_install 과정 변경

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# ECR 로그인
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository
 
-# Docker 컨테이너 정지 및 삭제
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
+# Deprecated
+# # ECR 로그인
+# aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository
 
-# ECR 이미지 가져오기
-docker pull 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository:latest
+# # Docker 컨테이너 정지 및 삭제
+# docker stop $(docker ps -a -q)
+# docker rm $(docker ps -a -q)
+# docker rmi $(docker images -q)
 
-# Docker 컨테이너 실행
-docker run -d -it --name developer_discovery -p 80:80 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository:latest
+# # ECR 이미지 가져오기
+# docker pull 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository:latest
 
-docker exec -i $(sudo docker ps -q -l) python developer_discover/manage.py migrate
+# # Docker 컨테이너 실행
+# docker run -d -it --name developer_discovery -p 80:80 164899418867.dkr.ecr.ap-northeast-2.amazonaws.com/developer_discovery_repository:latest
+
+# docker exec -i $(sudo docker ps -q -l) python developer_discover/manage.py migrate
+
+
+# Git pull
+git pull
+
+docker compose -f docker-compose-prod.yml up -d nginx api
